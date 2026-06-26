3b0e83f555b27793745f8613599417f9635a2d13
TithiSolanki17 <tithisolanki43@gmail.com>
2026-06-26 20:54:26 +0530

    Intentional conflicting on print statements

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is your ADMIN Account");
+        System.out.println("This is BUG FIXING BRANCH");
     }
 }
